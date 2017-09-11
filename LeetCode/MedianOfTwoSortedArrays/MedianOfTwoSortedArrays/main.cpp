aac5deb99a066dfd5d5c4394535f9522afc526ff
李茂琦 <limaoqi@meituan.com>
2017-09-11 12:55:36 +0800

    problem 4(第二遍)

--- LeetCode/MedianOfTwoSortedArrays/MedianOfTwoSortedArrays/main.cpp
+++ LeetCode/MedianOfTwoSortedArrays/MedianOfTwoSortedArrays/main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 class Solution {
 public:
+    // 45ms: O((m + n - 1) / 2)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = (int)nums1.size(), n = (int)nums2.size();
         int medianPos = (m + n - 1) / 2;
@@ -42,12 +43,46 @@ public:
         }
         return median;
     }
+    // 62ms: O(log(min(m, n)))
+    double findMedianSortedArrays1(vector<int>& nums1, vector<int>& nums2) {
+        int m  = (int)nums1.size(), n = (int)nums2.size();
+        if (m > n) return findMedianSortedArrays1(nums2, nums1);
+        int lo = 0, hi = m, halfLen = (m + n + 1) / 2;
+        while (lo <= hi) {
+            int i = (lo + hi) / 2, j = halfLen - i;
+            if (i >= 1 && nums1[i - 1] > nums2[j]) {
+                hi = i - 1;
+            } else if (i < m && nums2[j - 1] > nums1[i]) {
+                lo = i + 1;
+            } else {
+                int left = 0;
+                if (i == 0 && j > 0) {
+                    left = nums2[j - 1];
+                } else if (j == 0 && i > 0) {
+                    left = nums1[i - 1];
+                } else {
+                    left = max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2) return left;
+                int right = 0;
+                if (i == m && j < n) {
+                    right = nums2[j];
+                } else if (j == n && i < m) {
+                    right = nums1[i];
+                } else {
+                    right = min(nums1[i], nums2[j]);
+                }
+                return (left + right) / 2.0;
+            }
+        }
+        return 0.0;
+    }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    vector<int> nums1 = {1, 2};
-    vector<int> nums2 = {3, 4};
-    cout << Solution().findMedianSortedArrays(nums1, nums2) << endl;
+    vector<int> nums1 = {4, 5};
+    vector<int> nums2 = {1, 2, 3};
+    cout << Solution().findMedianSortedArrays1(nums1, nums2) << endl;
     return 0;
 }
