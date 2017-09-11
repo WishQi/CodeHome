40e2092bb534a30a131ea2aec8ce7225c977fe4c
李茂琦 <limaoqi@meituan.com>
2017-09-11 18:03:46 +0800

    problem 5(第二遍)

--- LeetCode/LongestPalindromicSubstring/LongestPalindromicSubstring/main.cpp
+++ LeetCode/LongestPalindromicSubstring/LongestPalindromicSubstring/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 // dynamic programming
 class Solution1 {
 public:
+    // 89ms, AC, O((n + 1) * n / 2)
     string longestPalindrome(string s) {
         if (s.length() <= 1) return s;
         bool dp[1001][1001] = { false };
@@ -32,42 +33,66 @@ public:
         }
         return s.substr(start, maxLength);
     }
+    // 82ms, AC, O((n + 1) * n / 2)
+    string longestPalindrome1(string s) {
+        int len = (int)s.length();
+        if (len <= 1) return s;
+        bool dp[1001][1001] = {false};
+        memset(dp, false, sizeof(dp));
+        int start = len - 1, maxLen = 0;
+        for (int i = len - 1; i >= 0; --i) {
+            for (int j = i; j < len; ++j) {
+                int tmpLen = j - i + 1;
+                dp[i][j] = s[i] == s[j] && (tmpLen <= 3 || dp[i + 1][j - 1]);
+                if (dp[i][j] && tmpLen > maxLen) {
+                    start = i;
+                    maxLen = tmpLen;
+                }
+            }
+        }
+        return s.substr(start, maxLen);
+    }
 };
 
 class Solution2 {
 public:
+    // 12ms, AC, O((n + 2) * n / 4)
     string longestPalindrome(string s) {
-        int length = (int)s.length();
-        int start = 0, maxLength = 1;
-        for (int i = 1; i < length; ++i) {
-            // even
-            int low = i - 1, high = i;
-            while (low >= 0 && high < length && s[low] == s[high]) {
-                --low;
-                ++high;
-            }
-            if (high - low - 1 > maxLength) {
-                start = low + 1;
-                maxLength = high - low - 1;
-            }
+        int len = (int)s.length();
+        if (len <= 1) return s;
+        int start = 0, maxLen = 1;
+        for (int i = 1; i < len; ++i) {
             // odd
-            low = i - 1, high = i + 1;
-            while (low >= 0 && high < length && s[low] == s[high]) {
-                --low;
-                ++high;
+            int lo = i - 1, hi = i;
+            while (lo >= 0 && hi < len && s[lo] == s[hi]) {
+                lo--;
+                hi++;
             }
-            if (high - low - 1 > maxLength) {
-                start = low + 1;
-                maxLength = high - low - 1;
+            int tmpLen = hi - lo - 1;
+            if (tmpLen > maxLen) {
+                start = lo + 1;
+                maxLen = tmpLen;
+            }
+            // even
+            lo = i - 1, hi = i + 1;
+            while (lo >= 0 && hi < len && s[lo] == s[hi]) {
+                lo--;
+                hi++;
+            }
+            tmpLen = hi - lo - 1;
+            if (tmpLen > maxLen) {
+                start = lo + 1;
+                maxLen = tmpLen;
             }
         }
-        return s.substr(start, maxLength);
+        return s.substr(start, maxLen);
     }
 };
 
 int main(int argc, const char * argv[]) {
     string s = "defgfed";
-    cin >> s;
-    cout << Solution1().longestPalindrome(s) << endl;
+    while (cin >> s) {
+        cout << Solution2().longestPalindrome(s) << endl;
+    }
     return 0;
 }
