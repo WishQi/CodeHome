77c14c4f6148c8584537955d73066f3a73301031
李茂琦 <limaoqi@meituan.com>
2017-09-22 11:45:38 +0800

    problem 10(第二遍)

--- LeetCode/10. Regular Expression Matching/RegularExpressionMatching/main.cpp
+++ LeetCode/10. Regular Expression Matching/RegularExpressionMatching/main.cpp
@@ -24,6 +24,16 @@ public:
             return (!s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p)) || isMatch(s, p.substr(2));
         }
     }
+    // 第二遍 89ms, AC
+    bool isMatch1(string s, string p) {
+        if (p.empty()) return s.empty();
+        if (p.size() == 1) return s.size() == 1 && (s[0] == p[0] || p[0] == '.');
+        if (p[1] != '*') {
+            return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        } else {
+            return (!s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p)) || isMatch(s, p.substr(2));
+        }
+    }
 };
 
 // dynamic programming
@@ -47,10 +57,28 @@ public:
         }
         return dp[m][n];
     }
+    // 第二遍 52ms, AC
+    bool isMatch1(string s, string p) {
+        bool dp[1001][1001] = {false};
+        int m = (int)s.size(), n = (int)p.size();
+        dp[0][0] = true;
+        for (int i = 1; i <= m; ++i) dp[i][0] = false;
+        for (int i = 1; i <= n; ++i) dp[0][i] = i > 1 && p[i - 1] == '*' && dp[0][i - 2];
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] || ((s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                }
+            }
+        }
+        return dp[m][n];
+    }
 };
 
 int main(int argc, const char * argv[]) {
-    string s, p;
+    string s = "", p = "a***";
     while (cin >> s >> p) {
         cout << Solution1().isMatch(s, p) << endl;
     }
