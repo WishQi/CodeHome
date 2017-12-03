d96b22f94289b80bc46db04c5f51fef577a6a768
WishQi <lmq729981607@gmail.com>
2017-12-03 11:35:05 +0800

    problem 538

---
+++ LeetCode/538. Convert BST to Greater Tree/538. Convert BST to Greater Tree/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  538. Convert BST to Greater Tree
+//
+//  Created by 李茂琦 on 03/12/2017.
+//  Copyright © 2017 李茂琦. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 39ms, AC
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        change(root, sum);
+        return root;
+    }
+    void change(TreeNode *root, int &sum) {
+        if (root != NULL) {
+            change(root->right, sum);
+            sum += root->val;
+            root->val = sum;
+            change(root->left, sum);
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
